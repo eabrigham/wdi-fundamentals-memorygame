f6d94cd0eb2fe2925e046369642dd19a37f42324
eabrigham <eabrigham@gmail.com>
2018-06-02 15:14:45 -0400

    Made the game interactive using the DOM and event listeners. Completed the General Assembly tutorials of the game.

--- js/main.js
+++ js/main.js
@@ -1,3 +1,14 @@
+/* Bonuses: Add reset button to the HTML and set up an event
+in the JavaScript file so the user can reset the game after playing.
+
+Extra challenge, consider how to keep track of and display the user's
+score.
+
+My challenge: how to tell the user if they found a match without using
+the console or those godawful alerts.
+*/
+
+// Creates an array of card object literals
 var cards = [
   {
     rank: "queen",
@@ -22,17 +33,7 @@ var cards = [
 ];
 var cardsInPlay = [];
 
-
-var flipCard = function(cardId) {
-  console.log("User flipped the " + cards[cardId].rank + ' of ' + cards[cardId].suit);
-  console.log(cards[cardId].cardImage);
-  cardsInPlay.push(cards[cardId]);
-  if (cardsInPlay.length === 2) {
-    //call compare function
-    console.log(compareCards(cardsInPlay[0].rank,cardsInPlay[1].rank));
-  }
-}
-
+// compares two cards to see if they match
 var compareCards = function(card1, card2) {
   if (card1 === card2) {
     return "You found a match!";
@@ -40,5 +41,25 @@ var compareCards = function(card1, card2) {
   return "No match, try again";
 }
 
-flipCard(0);
-flipCard(1);
+// flips a card and adds it to the cards in play
+var flipCard = function() {
+  var cardId = this.getAttribute('data-id');
+  console.log("User flipped the " + cards[cardId].rank + ' of ' + cards[cardId].suit);
+  this.setAttribute('src', cards[cardId].cardImage);
+  cardsInPlay.push(cards[cardId]);
+  if (cardsInPlay.length === 2) {
+    //call compare function
+    console.log(compareCards(cardsInPlay[0].rank,cardsInPlay[1].rank));
+  }
+}
+
+var createBoard = function() {
+  for (var i = 0; i < cards.length; i++) {
+    var cardElement = document.createElement('img');
+    cardElement.setAttribute('src', 'images/back.png');
+    cardElement.setAttribute('data-id', i);
+    cardElement.addEventListener('click', flipCard);
+    document.getElementById('game-board').appendChild(cardElement);
+  }
+}
+createBoard();
